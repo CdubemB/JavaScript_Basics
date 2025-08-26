9713d670e1266a2f945df721e989cdcfad99dc8e
CdubemB <dubemwb@gmail.com>
2025-08-26 16:38:17 +0100

    edits

--- Node/app.js
+++
@@ -1,5 +0,0 @@
-const lesson1 = require("./lesson1 Module_exports")
-
-console.log(lesson1.sum(2, 3));
-console.log(lesson1.PI);
-console.log(new lesson1.someMathObject());
--- Node/lesson2_Events.js
+++
@@ -1,6 +0,0 @@
-const EventEmitter = require("events");
-const eventEmitter = new EventEmitter();
-
-eventEmitter.on("lesson2", ()=>{
-  console.log("lesson2 event has occured")
-})
